37aa800d2ea77f992fccd457a5bd79891d9e6dba
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2020-05-20 02:11:44 +0430

    added post panel

---
+++ src/View/Panel/CreatePostModal.js
@@ -0,0 +1,144 @@
+import React, {PureComponent} from "react"
+import MaterialInput from "../Components/MaterialInput"
+import Material from "../Components/Material"
+import PencilSvg from "../../Media/Svgs/Pencil"
+import CameraSvg from "../../Media/Svgs/Camera"
+import {NotificationManager} from "react-notifications"
+import compressImage from "../../Helpers/compressImage"
+import api from "../../Functions/api"
+
+class CreatePostModal extends PureComponent
+{
+    constructor(props)
+    {
+        super(props)
+        this.state = {}
+    }
+
+    setValue = e =>
+    {
+        const {value, name} = e.target
+        this[name] = value
+    }
+
+    categorySelect = e => this.category_id = e.target.value
+
+    selectPicture = e =>
+    {
+        const img = e.target.files[0]
+        this.picture = img
+        const reader = new FileReader()
+        reader.readAsDataURL(img)
+        reader.onload = () => this.setState({...this.state, picturePreview: reader.result})
+        e.target.value = ""
+    }
+
+    submit = () =>
+    {
+        const title = this.title?.trim()
+        const short_description = this.short_description?.trim()
+        const category_id = this.category_id?.trim()
+        const picture = this.picture
+
+        if (title && title.length >= 5 && short_description && short_description.length >= 10 && category_id && category_id !== "0" && picture)
+        {
+            const form = new FormData()
+            form.append("title", title)
+            form.append("short_description", short_description)
+            form.append("category_id", category_id)
+            compressImage(picture).then(picture =>
+            {
+            form.append("picture", picture)
+            api.post("post", form, "", e => this.setState({...this.state, loadingPercent: Math.floor((e.loaded * 100) / e.total)}))
+                .then(category =>
+                {
+                    const {addOrUpdatePost, toggleCreateModal} = this.props
+                    NotificationManager.success("با موفقیت ایجاد شد!")
+                    addOrUpdatePost(category)
+                    toggleCreateModal()
+                })
+                .catch(e =>
+                {
+                    if (e?.response?.data?.keyPattern?.title) this.setState({...this.state, loading: false}, () => NotificationManager.error("عنوان وارد شده تکراری است!"))
+                    else this.setState({...this.state, loading: false}, () => NotificationManager.error("مشکلی پیش آمد! کانکشن خود را چک کنید!"))
+                })
+            })
+        }
+        else
+        {
+            if (!(title && title.length >= 5)) NotificationManager.warning("عنوانی وارد کنید که حداقل 5 کاراکتر باشد!")
+            if (!(short_description && short_description.length >= 10)) NotificationManager.warning("توضیحاتی وارد کنید که حداقل 10 کاراکتر باشد!")
+            if (!(category_id && category_id !== "0")) NotificationManager.warning("دسته بندی را انتخاب کنید!")
+            if (!(picture)) NotificationManager.warning("عکس را انتخاب کنید!")
+        }
+    }
+
+    render()
+    {
+        const {loading, loadingPercent, picturePreview} = this.state
+        const {toggleCreateModal, categories} = this.props
+        return (
+            <React.Fragment>
+                {
+                    loading &&
+                    <div className="sign-up-page-loading-cont override">
+                        <div className="panel-upload-percent">{loadingPercent} %</div>
+                    </div>
+                }
+                <div className="sign-up-page-loading-cont" onClick={loading ? null : toggleCreateModal}>
+                    <div className="sign-up-page modal" onClick={e => e.stopPropagation()}>
+                        <div className="sign-up-page-title">ساخت پست</div>
+                        <MaterialInput className="sign-up-page-field"
+                                       backgroundColor="var(--background-color)"
+                                       name="title"
+                                       maxLength={80}
+                                       label={<span>عنوان <span className="sign-up-page-field-star">*</span></span>}
+                                       getValue={this.setValue}
+                                       onKeyDown={this.submitOnEnter}
+                        />
+                        <MaterialInput className="sign-up-page-area"
+                                       isTextArea={true}
+                                       backgroundColor="var(--background-color)"
+                                       name="short_description"
+                                       maxLength={250}
+                                       label={<span>توضیحات کوتاه <span className="sign-up-page-field-star">*</span></span>}
+                                       getValue={this.setValue}
+                        />
+
+                        <select className="panel-select-box" onChange={this.categorySelect}>
+                            <option value="0">انتخاب دسته بندی *</option>
+                            {
+                                Object.values(categories).filter(cat => cat.parent_id).map(item =>
+                                    <option key={item._id} value={item._id}>{categories[item.parent_id].title} 🠘 {item.title}</option>,
+                                )
+                            }
+                        </select>
+
+                        <label className="panel-image-upload">
+                            <Material className="panel-image-upload-material">
+                                <div className="panel-image-upload-label">عکس اصلی</div>
+                                {
+                                    picturePreview ?
+                                        <React.Fragment>
+                                            <img className="panel-image-upload-img" src={picturePreview} alt=""/>
+                                            <PencilSvg className="panel-image-upload-edit"/>
+                                        </React.Fragment>
+                                        :
+                                        <React.Fragment>
+                                            <CameraSvg className="panel-image-upload-add"/>
+                                            <span className="sign-up-page-field-star">*</span>
+                                        </React.Fragment>
+                                }
+                                <input type="file" hidden accept="image/*" onChange={this.selectPicture}/>
+                            </Material>
+                        </label>
+
+                        <Material className={`login-modal-submit ${loading ? "loading" : ""}`} onClick={loading ? null : this.submit}>ثبت</Material>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default CreatePostModal
--- src/View/Panel/Posts.js
+++ src/View/Panel/Posts.js
@@ -1,12 +1,72 @@
 import React, {PureComponent} from "react"
+import {Switch} from "react-router-dom"
+import {ClipLoader} from "react-spinners"
+import Material from "../Components/Material"
+import CreatePostModal from "./CreatePostModal"
+import api from "../../Functions/api"
 
 class Posts extends PureComponent
 {
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            categories: {},
+            posts: {},
+        }
+    }
+
+    toggleCreateModal = () => this.setState({...this.state, openModal: !this.state.openModal})
+
+    componentDidMount()
+    {
+        api.get("post")
+            .then(posts => this.setState({...this.state, posts: posts.reduce((sum, post) => ({...sum, [post._id]: post}), {})}))
+
+        api.get("category")
+            .then(categories => this.setState({...this.state, categories: categories.reduce((sum, cat) => ({...sum, [cat._id]: cat}), {})}))
+    }
+
+    addOrUpdatePost = post =>
+    {
+        const {posts} = this.state
+        if (posts[post._id]) this.setState({...this.state, posts: {...posts, [post._id]: {...post}}})
+        else this.setState({...this.state, posts: {[post._id]: {...post}, ...posts}})
+    }
+
     render()
     {
+        const {error, isLoading, openModal, categories, posts} = this.state
         return (
-            <div>
-                ...Wait For Me
+            <div className="panel-categories-cont">
+                <Switch>
+                    <React.Fragment>
+                        <div className="panel-table-title">
+                            پست‌ها
+                        </div>
+                        {
+                            error ?
+                                <div className="panel-table-err-loading">مشکلی پیش آمد! کانکشن خود را بررسی کنید!</div>
+                                :
+                                Object.values(posts).length === 0 && !isLoading ? <div className="panel-table-err-loading">پستی یافت نشد!</div>
+                                    :
+                                    <div>
+                                        {
+                                            Object.values(posts).map(post =>
+                                                <div key={post._id}>{post.title}</div>,
+                                            )
+                                        }
+                                    </div>
+                        }
+
+                        {isLoading && <div className="panel-table-err-loading"><ClipLoader size={19} color="var(--primary-color)"/></div>}
+
+                        <Material className="panel-add-item" backgroundColor="rgba(255,255,255,0.3)" onClick={this.toggleCreateModal}>+</Material>
+
+                        {openModal && <CreatePostModal addOrUpdatePost={this.addOrUpdatePost} toggleCreateModal={this.toggleCreateModal} categories={categories}/>}
+
+                    </React.Fragment>
+                </Switch>
             </div>
         )
     }
